19e0ba22ae0ef2d36f728f99f7e790d0ae9c09c8
Mapi Garcia <mariepettejoycegarcia@gmail.com>
2021-10-27 23:20:58 +0800

    added a dynamic input setup for an object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const App = () => {
+  // stating and setting initial states
+  const [data, setData] = useState(0);
+  const [values, setValues] = useState({firstName: 'Ryan', lastName: 'N'});
+
+  // custom function code goes here
+  const clickedMeFunction = () => {
+    
+    setData(data+1)
+  }
+  
+  const handleChange = (e,requestField) => {
+    console.log(e.target.value);
+    setValues({...values, [requestField]: e.target.value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      {console.log(values)}
+      <h1>{data}</h1>
+      <h1>{values.firstName}</h1>
+      <input
+        type='text'
+        name='fname'
+        value={values.firstName}
+        onChange={e => {handleChange(e, 'firstName')}}
+      />
 
+      <button onClick={() => {clickedMeFunction()}}>Click Me!</button>
+    </div>
+  )
+}
 export default App;
